fe21bc71c265b7dbd56871752d30463978bcbcd0
Ywoosang <opellong13@gmail.com>
2024-07-22 14:07:47 +0900

    feat(YB-66): 공통 응답/에러가 Swagger 에 표시될 수 있도록 추가

--- yellobook-common/src/main/java/com/yellobook/response/BaseResponse.java
+++ yellobook-common/src/main/java/com/yellobook/response/BaseResponse.java
@@ -1,10 +1,17 @@
 package com.yellobook.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseResponse {
+    @JsonProperty("isSuccess")
+    @Schema(description = "요청이 성공했는지 여부", example = "true")
     Boolean isSuccess;
+
+    @JsonProperty("message")
+    @Schema(description = "추가 정보를 제공하는 메시지", example = "요청이 성공적으로 처리되었습니다.")
     String message;
 }
--- yellobook-common/src/main/java/com/yellobook/response/ErrorResponse.java
+++ yellobook-common/src/main/java/com/yellobook/response/ErrorResponse.java
@@ -2,21 +2,22 @@ package com.yellobook.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @JsonPropertyOrder({ "isSuccess", "code", "message", "errors" })
 public class ErrorResponse extends BaseResponse {
+    @Schema(name = "에러 코드")
     private final String code;
 
+    @Schema(name = "에러 필드 목록")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private final List<ValidationError> errors;
 
-    @Builder(access = AccessLevel.PROTECTED)
+    @Builder
     private ErrorResponse(String code, String message, List<ValidationError> errors) {
         super(false, message);
         this.code = code;
