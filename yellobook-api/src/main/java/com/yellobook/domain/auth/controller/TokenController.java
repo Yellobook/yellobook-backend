4a9a461b593e5ebd24b139450bc80dbaa4fe2317
Ywoosang <opellong13@gmail.com>
2024-07-22 14:05:48 +0900

    refactor(YB-64): JWT 통합을 위해 Oauth 방식 대신 HTTP Scheme 이용

--- yellobook-api/src/main/java/com/yellobook/domain/auth/controller/TokenController.java
+++ yellobook-api/src/main/java/com/yellobook/domain/auth/controller/TokenController.java
@@ -1,24 +1,26 @@
 package com.yellobook.domain.auth.controller;
 
 
+import com.yellobook.common.anonotation.ApiV1Controller;
 import com.yellobook.domain.auth.dto.TokenResponse;
 import com.yellobook.domain.auth.service.AuthService;
 import com.yellobook.response.ResponseFactory;
 import com.yellobook.response.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Tag(name = "\uD83D\uDD11 토큰", description = "Token API")
+@ApiV1Controller(value = "/token")
 @RequiredArgsConstructor
-@RequestMapping(value = "/api/v1/auth")
-public class AuthController {
+public class TokenController {
     private final AuthService authService;
 
-    @PostMapping("/token/reissue")
+    @Operation(summary = "accessToken 재발급")
+    @PostMapping("/reissue")
     public ResponseEntity<SuccessResponse<TokenResponse>> reissueToken(@RequestParam("token") String token) {
         TokenResponse tokenResponse = authService.reissueToken(token);
         return ResponseFactory.success(tokenResponse);
