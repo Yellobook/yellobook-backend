e77cc231449c582fe0bafbfb6ae5e994c32ef7cc
설승환 <tjftmd7@gmail.com>
2024-07-24 16:00:50 +0900

    feat(YB-68): 멤버 DTO 생성

--- yellobook-api/src/main/java/com/yellobook/domain/member/service/MemberQueryServiceImpl.java
+++ yellobook-api/src/main/java/com/yellobook/domain/member/service/MemberQueryServiceImpl.java
@@ -3,5 +3,6 @@ package com.yellobook.domain.member.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MemberService {
+public class MemberQueryServiceImpl implements MemberQueryService {
+
 }
