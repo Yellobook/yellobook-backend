29e02de55a060ddced7aa5b65d339c5f6571e799
Ywoosang <opellong13@gmail.com>
2024-07-28 12:09:18 +0900

    feat(YB-160): ScheduleService 구현

--- yellobook-api/src/main/java/com/yellobook/domain/schedule/dto/request/MonthlyParam.java
+++ yellobook-api/src/main/java/com/yellobook/domain/schedule/dto/request/MonthlyParam.java
@@ -3,16 +3,18 @@ package com.yellobook.domain.schedule.dto.request;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 
 @Getter
+@RequiredArgsConstructor
 @ParameterObject
 @Schema(description = "월별 일정 조회를 위한 파라미터")
 public class MonthlyParam {
     @Parameter(description = "년", example = "2024")
-    private int year;
+    private final int year;
 
     @Parameter(description = "월", example = "7")
-    private int month;
+    private final int month;
 }
 
