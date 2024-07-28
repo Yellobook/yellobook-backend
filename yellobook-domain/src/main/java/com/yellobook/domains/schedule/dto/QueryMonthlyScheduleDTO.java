a779372c6bfa1cb8a25cce9bcbeac10ae99106c8
Ywoosang <opellong13@gmail.com>
2024-07-28 13:44:23 +0900

    fix: domain 모듈 DTO 에서 swagger 애노테이션 삭제

--- yellobook-domain/src/main/java/com/yellobook/domains/schedule/dto/QueryMonthlyScheduleDTO.java
+++ yellobook-domain/src/main/java/com/yellobook/domains/schedule/dto/QueryMonthlyScheduleDTO.java
@@ -1,14 +1,9 @@
 package com.yellobook.domains.schedule.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NonNull;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @AllArgsConstructor
