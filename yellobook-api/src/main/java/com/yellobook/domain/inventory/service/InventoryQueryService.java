80e691021683d7a223a75277c9025e7fdb88eb4e
Ywoosang <opellong13@gmail.com>
2024-07-30 14:29:44 +0900

    refactor(YB-174): TeamMember 애노테이션을 사용하도록 변경 - 중복 코드 제거

--- yellobook-api/src/main/java/com/yellobook/domain/inventory/service/InventoryQueryService.java
+++ yellobook-api/src/main/java/com/yellobook/domain/inventory/service/InventoryQueryService.java
@@ -2,7 +2,8 @@ package com.yellobook.domain.inventory.service;
 
 import com.yellobook.common.enums.MemberTeamRole;
 import com.yellobook.common.utils.ParticipantUtil;
-import com.yellobook.common.utils.TeamUtil;
+import com.yellobook.common.vo.TeamMemberVO;
+import com.yellobook.domain.auth.service.RedisTeamService;
 import com.yellobook.domain.auth.security.oauth2.dto.CustomOAuth2User;
 import com.yellobook.domain.inventory.dto.GetProductsResponse;
 import com.yellobook.domain.inventory.dto.GetTotalInventoryResponse;
@@ -28,19 +29,16 @@ public class InventoryQueryService{
     private final ParticipantUtil participantUtil;
     private final InventoryMapper inventoryMapper;
     private final ProductMapper productMapper;
-    private final TeamUtil teamUtil;
+    private final RedisTeamService teamUtil;
 
     /**
      * 전체 재고 현황 확인 API (관리자, 주문자)
      */
-    public GetTotalInventoryResponse getTotalInventory(Integer page, Integer size, CustomOAuth2User oAuth2User) {
-        Long memberId = oAuth2User.getMemberId();
-        Long teamId  = teamUtil.getCurrentTeam(memberId);
-        MemberTeamRole role = participantUtil.getMemberTeamRole(teamId, memberId);
-        participantUtil.forbidViewer(role);
+    public GetTotalInventoryResponse getTotalInventory(Integer page, Integer size, TeamMemberVO teamMember) {
+        participantUtil.forbidViewer(teamMember.getRole());
 
         Pageable pageable = PageRequest.of(page-1, size);
-        List<InventoryDTO> content = inventoryRepository.getTotalInventory(teamId, pageable);
+        List<InventoryDTO> content = inventoryRepository.getTotalInventory(teamMember.getTeamId(), pageable);
         return GetTotalInventoryResponse.builder()
                 .page(page).size(content.size())
                 .inventories(inventoryMapper.toInventoryInfoList(content))
@@ -50,13 +48,10 @@ public class InventoryQueryService{
     /**
      * 특정 제고 일자 현황 조회 (관리자, 주문자)
      */
-    public GetProductsResponse getProductsByInventory(Long inventoryId, CustomOAuth2User oAuth2User) {
+    public GetProductsResponse getProductsByInventory(Long inventoryId, TeamMemberVO teamMember) {
         // 존재하는 재고 인지 확인, 접근 권환 확인
         // 제품 이름 순으로 정렬해서 보여주기 (페이징 X)
-        Long memberId = oAuth2User.getMemberId();
-        Long teamId  = teamUtil.getCurrentTeam(memberId);
-        MemberTeamRole role = participantUtil.getMemberTeamRole(teamId, memberId);
-        participantUtil.forbidViewer(role);
+        participantUtil.forbidViewer(teamMember.getRole());
 
         List<GetProductsResponse.ProductInfo> content = productMapper.toProductInfo(productRepository.getProducts(inventoryId));
         return GetProductsResponse.builder().products(content).build();
@@ -65,14 +60,11 @@ public class InventoryQueryService{
     /**
      * 재고 검색 (관리자)
      */
-    public GetProductsResponse getProductByKeywordAndInventory(Long inventoryId, String keyword, CustomOAuth2User oAuth2User) {
+    public GetProductsResponse getProductByKeywordAndInventory(Long inventoryId, String keyword, TeamMemberVO teamMember) {
         // 존재하는 재고인지 확인, 접근 권한 확인
         // 제품 이름 순으로 정렬해서 보여주기 (페이징 X)
-        Long memberId = oAuth2User.getMemberId();
-        Long teamId  = teamUtil.getCurrentTeam(memberId);
-        MemberTeamRole role = participantUtil.getMemberTeamRole(teamId, memberId);
-        participantUtil.forbidViewer(role);
-        participantUtil.forbidOrderer(role);
+        participantUtil.forbidViewer(teamMember.getRole());
+        participantUtil.forbidOrderer(teamMember.getRole());
 
         List<GetProductsResponse.ProductInfo> content = productMapper.toProductInfo(productRepository.getProducts(inventoryId, keyword));
         return GetProductsResponse.builder().products(content).build();
