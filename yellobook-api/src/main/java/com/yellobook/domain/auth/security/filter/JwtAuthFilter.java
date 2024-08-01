1035f683eb368c22b69db27c278c2b904539e907
Ywoosang <opellong13@gmail.com>
2024-08-02 02:14:28 +0900

    refactor(YB-194): JwtAuthFilter 에서 에러에 대한 응답로직 삭제

--- yellobook-api/src/main/java/com/yellobook/domain/auth/security/filter/JwtAuthFilter.java
+++ yellobook-api/src/main/java/com/yellobook/domain/auth/security/filter/JwtAuthFilter.java
@@ -1,21 +1,17 @@
 package com.yellobook.domain.auth.security.filter;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yellobook.domain.auth.security.oauth2.dto.CustomOAuth2User;
 import com.yellobook.domain.auth.security.oauth2.dto.OAuth2UserDTO;
 import com.yellobook.domain.auth.service.JwtService;
 import com.yellobook.domain.auth.service.RedisAuthService;
 import com.yellobook.domains.member.entity.Member;
 import com.yellobook.error.exception.CustomException;
-import com.yellobook.response.ResponseFactory;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -32,17 +28,13 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String accessToken = jwtService.resolveAccessToken(request);
-        OAuth2UserDTO oauth2UserDTO;
-
-        // 로그인한 사용자
         try {
-            if (accessToken != null
-                    && !jwtService.isAccessTokenExpired(accessToken)
+            String accessToken = jwtService.resolveAccessToken(request);
+            if (!jwtService.isAccessTokenExpired(accessToken)
                     && !redisAuthService.isTokenInBlacklist(accessToken)
             ) {
                 Member member = jwtService.getMemberFromAccessToken(accessToken);
-                oauth2UserDTO = OAuth2UserDTO.from(member);
+                OAuth2UserDTO oauth2UserDTO = OAuth2UserDTO.from(member);
                 CustomOAuth2User customOAuth2User = new CustomOAuth2User(oauth2UserDTO);
                 Authentication authentication = new UsernamePasswordAuthenticationToken(
                         customOAuth2User,
@@ -52,25 +44,12 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (CustomException ex) {
-            log.warn(ex.getMessage());
-            handleCustomException(response, ex);
-            return;
-        } catch (Error e) {
-            // 에러핸들러 처리 추가 요망
-            log.error(e.getMessage());
+             request.setAttribute("error", ex.getErrorCode());
+        } catch (Exception e) {
+            log.error("[AUTH_ERROR] 사용자 인가 과정에서 에러 발생: {}", e.getMessage());
+            request.setAttribute("error", null);
         }
         // 로그인하지 않은 사용자 SecurityContext 에 없으므로 403
         filterChain.doFilter(request, response);
-    }
-
-    private void handleCustomException(HttpServletResponse response, CustomException e) throws IOException {
-        ResponseEntity<Object> errorResponse = ResponseFactory.failure(e.getErrorCode());
-        /**
-         *  charset=UTF-8 을 붙이지 않으면 message 가 ??? 로 깨져서 표시된다.
-         */
-        response.setContentType("application/json; charset=UTF-8");
-        response.setStatus(errorResponse.getStatusCode().value());
-        response.getWriter().write(new ObjectMapper().writeValueAsString(errorResponse.getBody()));
-        response.getWriter().flush();
     }
 }
