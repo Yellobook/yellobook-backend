bf87f57c4f7ab19dda9c29b136b5275017c133d3
Ywoosang <opellong13@gmail.com>
2024-08-05 11:11:40 +0900

    refactor(YB-216): 약관 동의 후 리다이렉트 하도록 변경

--- yellobook-api/src/main/java/com/yellobook/domain/auth/controller/AuthController.java
+++ yellobook-api/src/main/java/com/yellobook/domain/auth/controller/AuthController.java
@@ -1,9 +1,7 @@
 package com.yellobook.domain.auth.controller;
 
 import com.yellobook.domain.auth.dto.request.AllowanceRequest;
-import com.yellobook.domain.auth.dto.request.LogoutRequest;
-import com.yellobook.domain.auth.dto.response.AllowanceResponse;
-import com.yellobook.domain.auth.dto.response.TokenResponse;
+import com.yellobook.domain.auth.dto.response.RefreshResponse;
 import com.yellobook.domain.auth.security.oauth2.dto.CustomOAuth2User;
 import com.yellobook.domain.auth.service.AuthService;
 import com.yellobook.domain.auth.service.CookieService;
@@ -13,24 +11,34 @@ import com.yellobook.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 @Tag(name = "\uD83D\uDD11 인증", description = "Auth API")
 public class AuthController {
+
+    @Value("${frontend.auth-redirect-url}")
+    private String authRedirectUrl;
+
     private final AuthService authService;
     private final CookieService cookieService;
     private final JwtService jwtService;
 
     @Operation(summary = "accessToken 재발급")
     @PostMapping("/token/reissue")
-    public ResponseEntity<SuccessResponse<TokenResponse>> reissueToken(
+    public ResponseEntity<SuccessResponse<RefreshResponse>> reissueToken(
             HttpServletRequest request
     ) {
         String refreshToken = cookieService.getRefreshToken(request);
@@ -40,21 +48,23 @@ public class AuthController {
 
     @Operation(summary = "약관 동의")
     @PostMapping("/terms")
-    public ResponseEntity<SuccessResponse<AllowanceResponse>> agreeToTerms(
-            @Valid @RequestBody AllowanceRequest request
-    ) {
+    public void agreeToTerms(
+            @Valid @RequestBody AllowanceRequest request,
+            HttpServletResponse response
+    ) throws IOException {
         var result = authService.updateAllowance(request.getToken());
-        return ResponseFactory.success(result);
+        log.info(result.toString());
+        response.addCookie(cookieService.createAccessTokenCookie(result.accessToken()));
+        response.addCookie(cookieService.createRefreshTokenCookie(result.refreshToken()));
+        response.sendRedirect(authRedirectUrl);
     }
-
 
     @Operation(summary = "로그아웃")
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(
-            @Valid @RequestBody LogoutRequest request,
             @AuthenticationPrincipal CustomOAuth2User oAuth2User
     ) {
-        authService.logout(request.getAccessToken(), oAuth2User.getMemberId());
+//        authService.logout(oAuth2User.getMemberId());
         return ResponseFactory.noContent();
     }
 
