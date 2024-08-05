4b97f62812d0f543a99d9f1fdd3a286667834ac2
Ywoosang <opellong13@gmail.com>
2024-08-05 10:30:40 +0900

    feat(YB-212): 약관 동의 요청 DTO 에 제약조건 추가

--- yellobook-api/src/main/java/com/yellobook/domain/auth/dto/request/AllowanceRequest.java
+++ yellobook-api/src/main/java/com/yellobook/domain/auth/dto/request/AllowanceRequest.java
@@ -1,11 +1,13 @@
 package com.yellobook.domain.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 @Schema(description = "약관 동의 요청")
 public class AllowanceRequest {
+    @NotBlank(message = "약관 동의 토큰이 존재하지 않습니다.")
     @Schema(description = "약관 동의 토큰")
     private String token;
 }
