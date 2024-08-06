dea7fcd9bc66b0e6412231013440753020f2a581
jorippppong <duelee75@gmail.com>
2024-08-06 18:17:21 +0900

    feat(YB-244): 이메일로 사용자 존재 여부 확인 Unit Test

---
+++ yellobook-domain/src/test/java/com/yellobook/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.yellobook.domain.member;
+
+import com.yellobook.common.enums.MemberRole;
+import com.yellobook.config.TestConfig;
+import com.yellobook.domains.member.entity.Member;
+import com.yellobook.domains.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ContextConfiguration(classes = TestConfig.class)
+@DisplayName("Member 도메인 Repository Unit Test")
+public class MemberRepositoryTest {
+    private final MemberRepository memberRepository;
+    private Member member;
+
+    @Autowired
+    public MemberRepositoryTest(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    @BeforeEach
+    void setMember(){
+        member = Member.builder()
+                .nickname("nickname1")
+                .email("email1")
+                .profileImage("image1")
+                .role(MemberRole.USER)
+                .allowance(false)
+                .build();
+    }
+
+    @Test
+    @DisplayName("이메일로 존재하는 사용자 찾기")
+    void getExistMember(){
+        //given
+        String email = member.getEmail();
+        memberRepository.save(member);
+
+        //when
+        Optional<Member> optionalMember = memberRepository.findByEmail(email);
+
+        //then
+        Assertions.assertThat(optionalMember).isPresent();
+        Assertions.assertThat(optionalMember.get().getId()).isEqualTo(member.getId());
+        Assertions.assertThat(optionalMember.get().getEmail()).isEqualTo(member.getEmail());
+    }
+
+    @Test
+    @DisplayName("이메일로 존재하는 사용자가 존재 하지 않음을 확인")
+    void getNotExistMember(){
+        //given
+        String email = member.getEmail();
+
+        //when
+        Optional<Member> optionalMember = memberRepository.findByEmail(email);
+
+        //then
+        Assertions.assertThat(optionalMember).isEmpty();
+    }
+}
