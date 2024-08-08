54155a47e83f8c3edcac38869a7f2cc9d170f3b6
jorippppong <duelee75@gmail.com>
2024-08-09 03:23:16 +0900

    feat(YB-295): 주문 댓글 추가&조회 Unit Test

--- yellobook-api/src/test/java/com/yellobook/domain/order/controller/OrderControllerTest.java
+++ yellobook-api/src/test/java/com/yellobook/domain/order/controller/OrderControllerTest.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yellobook.common.enums.MemberTeamRole;
 import com.yellobook.common.resolver.TeamMemberArgumentResolver;
 import com.yellobook.common.vo.TeamMemberVO;
+import com.yellobook.domain.order.dto.request.AddOrderCommentRequest;
 import com.yellobook.domain.order.dto.request.MakeOrderRequest;
+import com.yellobook.domain.order.dto.response.AddOrderCommentResponse;
+import com.yellobook.domain.order.dto.response.GetOrderCommentsResponse;
 import com.yellobook.domain.order.dto.response.GetOrderResponse;
 import com.yellobook.domain.order.dto.response.MakeOrderResponse;
 import com.yellobook.domain.order.service.OrderCommandService;
@@ -22,6 +25,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -103,6 +108,45 @@ class OrderControllerTest {
                 .andExpect(status().isBadRequest())
                 .andReturn();
     }
+
+    @Test
+    @DisplayName("주문에 댓글 달기 - 주문이 존재 할 때")
+    void addOrderComment() throws Exception{
+        //given
+        Long orderId = 1L;
+        AddOrderCommentRequest request = AddOrderCommentRequest.builder().content("content").build();
+        AddOrderCommentResponse response = AddOrderCommentResponse.builder().commentId(1L).build();
+        when(orderQueryService.existsByOrderId(orderId)).thenReturn(true);
+        when(orderCommandService.addOrderComment(orderId, teamMemberVO, request)).thenReturn(response);
+
+        //when & then
+        mockMvc.perform(post("/api/v1/orders/{orderId}/comment", orderId)
+                .content(objectMapper.writeValueAsString(request))
+                .contentType("application/json;charset=UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.commentId", CoreMatchers.is(response.commentId().intValue())))
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("주문 댓글 조회")
+    void getOrderComments() throws Exception{
+        //given
+        Long orderId = 1L;
+        GetOrderCommentsResponse response = GetOrderCommentsResponse.builder().comments(Collections.emptyList()).build();
+        when(orderQueryService.existsByOrderId(orderId)).thenReturn(true);
+        when(orderQueryService.getOrderComments(orderId, teamMemberVO)).thenReturn(response);
+
+        //when & then
+        mockMvc.perform(get("/api/v1/orders/{orderId}/comment", orderId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.comments", CoreMatchers.is(response.comments())))
+                .andReturn();
+    }
+
+
 
 
 
