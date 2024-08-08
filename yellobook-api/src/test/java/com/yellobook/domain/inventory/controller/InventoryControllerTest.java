cda879aaf213781dc9faf7d0bf7b56b8fc360e08
jorippppong <duelee75@gmail.com>
2024-08-09 01:50:50 +0900

    feat(YB-288): 재고 상세 조회 Unit Test

--- yellobook-api/src/test/java/com/yellobook/domain/inventory/controller/InventoryControllerTest.java
+++ yellobook-api/src/test/java/com/yellobook/domain/inventory/controller/InventoryControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yellobook.common.enums.MemberTeamRole;
 import com.yellobook.common.resolver.TeamMemberArgumentResolver;
 import com.yellobook.common.vo.TeamMemberVO;
+import com.yellobook.domain.inventory.dto.response.GetProductsResponse;
 import com.yellobook.domain.inventory.dto.response.GetTotalInventoryResponse;
 import com.yellobook.domain.inventory.service.InventoryCommandService;
 import com.yellobook.domain.inventory.service.InventoryQueryService;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.convert.DataSizeUnit;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -26,6 +28,7 @@ import java.util.Collections;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(InventoryController.class)
@@ -70,6 +73,47 @@ class InventoryControllerTest {
                 .andExpect(jsonPath("$.data.size", CoreMatchers.is(response.size())))
                 .andReturn();
     }
+
+    @Test
+    @DisplayName("일별 재고 현황 상세 조회 - 재고가 존재하는 경우")
+    void getProductsByInventory() throws Exception{
+        //given
+        Long inventoryId = 1L;
+        GetProductsResponse response = GetProductsResponse.builder().products(Collections.emptyList()).build();
+        when(inventoryQueryService.getProductsByInventory(inventoryId, teamMemberVO)).thenReturn(response);
+        when(inventoryQueryService.existByInventoryId(inventoryId)).thenReturn(true);
+
+        //when & then
+        mockMvc.perform(get("/api/v1/inventories/{inventoryId}", inventoryId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.products", CoreMatchers.is(response.products())))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("재고 검색 - 재고가 존재하는 경우")
+    void getProductByKeywordAndInventory() throws Exception{
+        //given
+        Long inventoryId = 1L;
+        String keyword = "product";
+        GetProductsResponse response = GetProductsResponse.builder().products(Collections.emptyList()).build();
+        when(inventoryQueryService.getProductByKeywordAndInventory(inventoryId, keyword, teamMemberVO)).thenReturn(response);
+        when(inventoryQueryService.existByInventoryId(inventoryId)).thenReturn(true);
+
+        //when & then
+        mockMvc.perform(get("/api/v1/inventories/{inventoryId}/search", inventoryId)
+                .queryParam("keyword", keyword)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.products", CoreMatchers.is(response.products())))
+                .andReturn();
+    }
+
+
+
+
+
 
 
 
