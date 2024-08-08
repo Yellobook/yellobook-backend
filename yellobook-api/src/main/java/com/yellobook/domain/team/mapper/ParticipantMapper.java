7672a3db19eae122feff7c9ccd841debbd39b5f2
설승환 <tjftmd7@gmail.com>
2024-08-08 18:05:01 +0900

    fix(YB-251): ParticipantMapper 내의 builder id 제거

--- yellobook-api/src/main/java/com/yellobook/domain/team/mapper/ParticipantMapper.java
+++ yellobook-api/src/main/java/com/yellobook/domain/team/mapper/ParticipantMapper.java
@@ -7,23 +7,17 @@ import com.yellobook.domains.team.entity.Team;
 import com.yellobook.common.enums.MemberTeamRole;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ParticipantMapper {
-    ParticipantMapper INSTANCE = Mappers.getMapper(ParticipantMapper.class);
+    @Mapping(source = "role", target = "role")
+    Participant toParticipant(MemberTeamRole role, Team team, Member member);
 
-    @Mapping(source = "team",target = "team")
-    @Mapping(source = "member",target = "member")
-    @Mapping(source = "memberTeamRole",target = "role")
-    @Mapping(target = "id", ignore = true)
-    Participant toParticipant(MemberTeamRole memberTeamRole, Team team, Member member);
-
-    @Mapping(source ="member.id", target = "id")
-    @Mapping(source ="member.nickname", target = "nickname")
-    MentionDTO toMentionDTO(Participant participants);
+    @Mapping(source ="participant.id", target = "id")
+    @Mapping(source ="participant.member.nickname", target = "nickname")
+    MentionDTO toMentionDTO(Participant participant);
 
     List<MentionDTO> toMentionDTOlist(List<Participant> participants);
 }
