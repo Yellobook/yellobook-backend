e899cb3f2246a966116051717a8f6b76bb0982f5
Ywoosang <opellong13@gmail.com>
2024-08-11 19:07:29 +0900

    fix(YB-320): @NotBlank 는 int 타입에 사용할 수 없음 - UnexpectedTypeException

--- yellobook-api/src/main/java/com/yellobook/domains/schedule/dto/request/MonthlyParam.java
+++ yellobook-api/src/main/java/com/yellobook/domains/schedule/dto/request/MonthlyParam.java
@@ -2,6 +2,8 @@ package com.yellobook.domains.schedule.dto.request;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import org.springdoc.core.annotations.ParameterObject;
@@ -10,10 +12,12 @@ import org.springdoc.core.annotations.ParameterObject;
 @ParameterObject
 @Schema(description = "월별 일정 조회를 위한 파라미터")
 public record MonthlyParam(
-        @NotBlank(message = "년도를 입력해 주세요.")
+        @Min(value = 0, message = "유효한 년도를 입력해 주세요.")
         @Parameter(description = "년", example = "2024")
         int year,
-        @NotBlank(message = "월을 입력해 주세요.")
+
+        @Min(value = 1, message = "유효한 월을 입력해 주세요.")
+        @Max(value = 12, message = "유효한 월을 입력해 주세요.")
         @Parameter(description = "월", example = "7")
         int month
 ) {
