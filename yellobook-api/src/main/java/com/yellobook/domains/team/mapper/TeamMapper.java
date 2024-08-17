0d5c6e0a12dbf6d1f72dde5f22877a659e4e708d
Ywoosang <opellong13@gmail.com>
2024-08-17 18:14:06 +0900

    feat(YB-374): api 모듈 팀 멤버 검색기능 추가 - controller - service - dto - mapper

--- yellobook-api/src/main/java/com/yellobook/domains/team/mapper/TeamMapper.java
+++ yellobook-api/src/main/java/com/yellobook/domains/team/mapper/TeamMapper.java
@@ -1,11 +1,14 @@
 package com.yellobook.domains.team.mapper;
 
+import com.yellobook.domains.team.dto.query.QueryTeamMember;
 import com.yellobook.domains.team.dto.request.CreateTeamRequest;
 import com.yellobook.domains.team.dto.response.*;
 import com.yellobook.domains.team.entity.Team;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Mapper(componentModel = "spring")
@@ -14,10 +17,18 @@ public interface TeamMapper {
 
     @Mapping(source = "id", target = "teamId")
     CreateTeamResponse toCreateTeamResponse(Team team);
+
     LeaveTeamResponse toLeaveTeamResponse(Long teamId);
+
     InvitationCodeResponse toInvitationCodeResponse(String inviteUrl);
+
     @Mapping(source = "id", target = "teamId")
     JoinTeamResponse toJoinTeamResponse(Team team);
+
     @Mapping(source = "id", target = "teamId")
     GetTeamResponse toGetTeamResponse(Team team);
+
+    default TeamMemberListResponse toTeamMemberListResponse(List<QueryTeamMember> members) {
+        return new TeamMemberListResponse(members);
+    }
 }
