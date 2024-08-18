6b47d604dc0c8fba130284c32f62fefeadf74d6f
설승환 <tjftmd7@gmail.com>
2024-08-18 16:05:22 +0900

    test(YB-315): 팀 생성 및 불러오기 Unit Test

---
+++ yellobook-api/src/test/java/com/yellobook/domains/team/controller/TeamControllerTest.java
@@ -0,0 +1,124 @@
+package com.yellobook.domains.team.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yellobook.common.enums.MemberRole;
+import com.yellobook.common.enums.MemberTeamRole;
+import com.yellobook.common.resolver.TeamMemberArgumentResolver;
+import com.yellobook.common.vo.TeamMemberVO;
+import com.yellobook.domains.auth.security.oauth2.dto.CustomOAuth2User;
+import com.yellobook.domains.auth.security.oauth2.dto.OAuth2UserDTO;
+import com.yellobook.domains.auth.service.RedisTeamService;
+import com.yellobook.domains.member.entity.Member;
+import com.yellobook.domains.team.dto.MentionDTO;
+import com.yellobook.domains.team.dto.request.InvitationCodeRequest;
+import com.yellobook.domains.team.dto.request.CreateTeamRequest;
+import com.yellobook.domains.team.dto.response.GetTeamResponse;
+import com.yellobook.domains.team.dto.response.InvitationCodeResponse;
+import com.yellobook.domains.team.dto.response.CreateTeamResponse;
+import com.yellobook.domains.team.dto.response.JoinTeamResponse;
+import com.yellobook.domains.team.service.TeamCommandService;
+import com.yellobook.domains.team.service.TeamQueryService;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = TeamController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class TeamControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private TeamCommandService teamCommandService;
+    @MockBean
+    private TeamQueryService teamQueryService;
+    @MockBean
+    private RedisTeamService redisTeamService;
+
+    @Mock
+    private CustomOAuth2User customOAuth2User;
+
+    @MockBean
+    private TeamMemberArgumentResolver teamMemberArgumentResolver;
+
+
+    private final TeamMemberVO teamMemberVO = TeamMemberVO.of(1L, 1L, MemberTeamRole.ADMIN);
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Member member = Member.builder()
+                .memberId(1L)
+                .nickname("test")
+                .email("test@example.com")
+                .profileImage("profileImage.png")
+                .allowance(true)
+                .role(MemberRole.USER)
+                .build();
+
+        OAuth2UserDTO oauth2UserDTO = OAuth2UserDTO.from(member);
+
+        customOAuth2User = new CustomOAuth2User(oauth2UserDTO);
+    }
+
+    @Test
+    @DisplayName("팀 생성")
+    public void createTeam_Success() throws Exception {
+        // Given
+        CreateTeamRequest request = new CreateTeamRequest("nike", "01000000000", "경기도", MemberTeamRole.ADMIN);
+        CreateTeamResponse response = new CreateTeamResponse(1L, LocalDateTime.now());
+
+        when(teamCommandService.createTeam(any(CreateTeamRequest.class), any(CustomOAuth2User.class)))
+                .thenReturn(response);
+
+        // when & then
+        mockMvc.perform(post("/api/v1/teams")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .principal(() -> customOAuth2User.getName())
+                )
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("팀 불러오기")
+    public void getTeamTest() throws Exception {
+        //given
+        Long teamId = 1L;
+        GetTeamResponse response = GetTeamResponse.builder().teamId(1L).name("나이키").phoneNumber("01000000000").address("경기도").build();
+
+        when(teamQueryService.existsByTeamId(teamId)).thenReturn(true);
+        when(teamQueryService.findByTeamId(any(), any())).thenReturn(response);
+
+        mockMvc.perform(get("/api/v1/teams/{teamId}", teamId)
+                        .content(objectMapper.writeValueAsString(response))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.teamId", CoreMatchers.is(response.teamId().intValue())))
+                .andDo(print());
+    }
+}
