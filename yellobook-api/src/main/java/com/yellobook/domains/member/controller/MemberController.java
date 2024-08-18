2d5dce23ed463d5d8a799476fa7fceb10002580e
jorippppong <duelee75@gmail.com>
2024-08-18 20:27:38 +0900

    fix(YB-391): 사용자 프로필 조회 컨트롤러 @AuthenticationPrincipal 로 변경

--- yellobook-api/src/main/java/com/yellobook/domains/member/controller/MemberController.java
+++ yellobook-api/src/main/java/com/yellobook/domains/member/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.yellobook.domains.member.controller;
 
-import com.yellobook.common.annotation.TeamMember;
-import com.yellobook.common.vo.TeamMemberVO;
+import com.yellobook.domains.auth.security.oauth2.dto.CustomOAuth2User;
 import com.yellobook.domains.member.dto.response.ProfileResponse;
 import com.yellobook.domains.member.service.MemberQueryService;
 import com.yellobook.response.ResponseFactory;
@@ -10,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +25,9 @@ public class MemberController {
     @GetMapping("/profile")
     @Operation(summary = "마이프로필 조회", description ="로그인을 한 멤버가 마이프로필을 조회하는 API입니다.")
     public ResponseEntity<SuccessResponse<ProfileResponse>> getMemberProfile(
-            @TeamMember TeamMemberVO teamMember
-    ){
-        ProfileResponse response = memberQueryService.getMemberProfile(teamMember.getMemberId());
+            @AuthenticationPrincipal CustomOAuth2User user
+            ){
+        ProfileResponse response = memberQueryService.getMemberProfile(user.getMemberId());
         return ResponseFactory.success(response);
     }
 }
--- yellobook-api/src/test/java/com/yellobook/domains/member/controller/MemberControllerTest.java
+++ yellobook-api/src/test/java/com/yellobook/domains/member/controller/MemberControllerTest.java
@@ -1,10 +1,16 @@
 package com.yellobook.domains.member.controller;
 
+import com.yellobook.common.enums.MemberRole;
 import com.yellobook.common.enums.MemberTeamRole;
 import com.yellobook.common.resolver.TeamMemberArgumentResolver;
 import com.yellobook.common.vo.TeamMemberVO;
+import com.yellobook.domains.auth.security.oauth2.dto.CustomOAuth2User;
+import com.yellobook.domains.auth.security.oauth2.dto.OAuth2UserDTO;
+import com.yellobook.domains.auth.service.RedisTeamService;
 import com.yellobook.domains.member.dto.response.ProfileResponse;
+import com.yellobook.domains.member.entity.Member;
 import com.yellobook.domains.member.service.MemberQueryService;
+import org.apache.catalina.User;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,16 +22,22 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.security.Principal;
 import java.util.Collections;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @WebMvcTest(MemberController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
@@ -39,12 +51,23 @@ class MemberControllerTest {
     @MockBean
     private TeamMemberArgumentResolver teamMemberArgumentResolver;
 
-    private final TeamMemberVO teamMemberVO = TeamMemberVO.of(1L, 1L, MemberTeamRole.ADMIN);
+    private Authentication authentication;
+
     @BeforeEach
     void setTeamMemberVO() throws Exception{
-        when(teamMemberArgumentResolver.supportsParameter(any())).thenReturn(true);
-        when(teamMemberArgumentResolver.resolveArgument(any(), any(), any(), any()))
-                .thenReturn(teamMemberVO);
+        Member member = Member.builder()
+                .memberId(1L)
+                .nickname("yellow")
+                .email("email")
+                .profileImage("image")
+                .allowance(true)
+                .role(MemberRole.USER)
+                .build();
+
+        OAuth2UserDTO oauth2UserDTO = OAuth2UserDTO.from(member);
+        CustomOAuth2User user = new CustomOAuth2User(oauth2UserDTO);
+        authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     @Test
@@ -52,11 +75,14 @@ class MemberControllerTest {
     void getMemberProfile() throws Exception{
         //given
         ProfileResponse response = new ProfileResponse(1L, "yellow", "image", "email", Collections.emptyList());
-        when(memberQueryService.getMemberProfile(teamMemberVO.getMemberId())).thenReturn(response);
+        when(memberQueryService.getMemberProfile(any(Long.class))).thenReturn(response);
 
         //when & then
         mockMvc.perform(get("/api/v1/members/profile")
-                .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .principal(authentication)
+                )
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.nickname", CoreMatchers.is(response.nickname())))
                 .andReturn();
