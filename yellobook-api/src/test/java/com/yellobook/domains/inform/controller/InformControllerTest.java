9d496dffe3274b444b12600c15b2d7700d24a79b
설승환 <tjftmd7@gmail.com>
2024-09-13 14:48:09 +0900

    style: 테스트 독립성을 위한 구조 변경

--- yellobook-api/src/test/java/com/yellobook/domains/inform/controller/InformControllerTest.java
+++ yellobook-api/src/test/java/com/yellobook/domains/inform/controller/InformControllerTest.java
@@ -1,5 +1,17 @@
 package com.yellobook.domains.inform.controller;
 
+import static fixture.MemberFixture.createMember;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yellobook.common.resolver.TeamMemberArgumentResolver;
 import com.yellobook.domains.auth.security.oauth2.dto.CustomOAuth2User;
@@ -13,7 +25,9 @@ import com.yellobook.domains.inform.repository.InformRepository;
 import com.yellobook.domains.inform.service.InformCommandService;
 import com.yellobook.domains.inform.service.InformQueryService;
 import com.yellobook.domains.member.entity.Member;
-import fixture.MemberFixture;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -26,17 +40,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(InformController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -57,41 +60,38 @@ public class InformControllerTest {
     @MockBean
     private TeamMemberArgumentResolver teamMemberArgumentResolver;
 
-    Member member;
-    CustomOAuth2User customOAuth2User;
-
-    private final Long informId = 1L;
-
-    @BeforeEach
-    void setUp() throws Exception {
-        member = MemberFixture.createMember();
-
-        OAuth2UserDTO oAuth2UserDTO = OAuth2UserDTO.from(member);
-        customOAuth2User = new CustomOAuth2User(oAuth2UserDTO);
-
-        when(teamMemberArgumentResolver.supportsParameter(any())).thenReturn(true);
-        when(teamMemberArgumentResolver.resolveArgument(any(), any(), any(), any()))
-                .thenReturn(customOAuth2User);
-    }
-
     @Nested
     @DisplayName("ExistInform 애노테이션은")
-    class Describe_ExistInform_Annotation{
+    class Describe_ExistInform_Annotation {
 
         @Nested
         @DisplayName("Inform이 존재하는 경우")
-        class Context_Exist_Inform{
+        class Context_Exist_Inform {
+
+            Member member;
+            CustomOAuth2User customOAuth2User;
+            Long informId;
 
             @BeforeEach
-            void setUp() {
+            void setUp() throws Exception {
+                member = createMember();
+                OAuth2UserDTO oAuth2UserDTO = OAuth2UserDTO.from(member);
+                customOAuth2User = new CustomOAuth2User(oAuth2UserDTO);
+
+                informId = 1L;
+
+                when(teamMemberArgumentResolver.supportsParameter(any())).thenReturn(true);
+                when(teamMemberArgumentResolver.resolveArgument(any(), any(), any(), any()))
+                        .thenReturn(customOAuth2User);
+
                 when(informRepository.existsById(informId)).thenReturn(true);
             }
 
             @Test
             @DisplayName("200 OK를 반환한다.")
-            void it_returns_200_ok()throws Exception{
+            void it_returns_200_ok() throws Exception {
                 mockMvc.perform(get("/api/v1/informs/{informId}", informId)
-                        .contentType(MediaType.APPLICATION_JSON))
+                                .contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().isOk())
                         .andReturn();
 
@@ -100,7 +100,7 @@ public class InformControllerTest {
 
         @Nested
         @DisplayName("Inform이 존재하지 않는 경우")
-        class Context_Not_Exist_Inform{
+        class Context_Not_Exist_Inform {
 
             Long nonExistInformId;
 
@@ -113,9 +113,9 @@ public class InformControllerTest {
 
             @Test
             @DisplayName("400 Bad Request를 반환한다.")
-            void it_returns_400_bad_request()throws Exception{
+            void it_returns_400_bad_request() throws Exception {
                 mockMvc.perform(get("/api/v1/informs/{informId}", nonExistInformId)
-                        .contentType(MediaType.APPLICATION_JSON))
+                                .contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().isBadRequest())
                         .andReturn();
             }
@@ -124,26 +124,38 @@ public class InformControllerTest {
 
     @Nested
     @DisplayName("createInform 메소드는")
-    class Describe_createInform{
+    class Describe_createInform {
 
         @Nested
         @DisplayName("유효한 요청의 경우")
         class Context_Valid_Request {
 
+            Long informId;
             CreateInformRequest request;
             CreateInformResponse response;
+            Member member;
+            CustomOAuth2User customOAuth2User;
 
             @BeforeEach
-            void setUp() {
+            void setUp() throws Exception {
+                informId = 1L;
                 request = new CreateInformRequest("test1", "01012345678", List.of(), LocalDate.now());
-                response = new CreateInformResponse(1L, LocalDateTime.now());
+                response = new CreateInformResponse(informId, LocalDateTime.now());
 
-                when(informCommandService.createInform(1L, request)).thenReturn(response);
+                member = createMember();
+                OAuth2UserDTO oAuth2UserDTO = OAuth2UserDTO.from(member);
+                customOAuth2User = new CustomOAuth2User(oAuth2UserDTO);
+
+                when(teamMemberArgumentResolver.supportsParameter(any())).thenReturn(true);
+                when(teamMemberArgumentResolver.resolveArgument(any(), any(), any(), any()))
+                        .thenReturn(customOAuth2User);
+
+                when(informCommandService.createInform(member.getId(), request)).thenReturn(response);
             }
 
             @Test
             @DisplayName("201 Created를 반환한다.")
-            void it_returns_201_created()throws Exception{
+            void it_returns_201_created() throws Exception {
                 mockMvc.perform(post("/api/v1/informs")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(objectMapper.writeValueAsString(request)))
@@ -155,21 +167,36 @@ public class InformControllerTest {
 
     @Nested
     @DisplayName("deleteInform 메소드는")
-    class Describe_deleteInform{
+    class Describe_deleteInform {
 
         @Nested
         @DisplayName("inform이 존재하는 경우")
-        class Context_Exist_Inform{
+        class Context_Exist_Inform {
+
+            Long informId;
+            Member member;
+            CustomOAuth2User customOAuth2User;
 
             @BeforeEach
-            void setUp() {
+            void setUp() throws Exception {
+                informId = 1L;
+
+                member = createMember();
+                OAuth2UserDTO oAuth2UserDTO = OAuth2UserDTO.from(member);
+                customOAuth2User = new CustomOAuth2User(oAuth2UserDTO);
+
+                when(teamMemberArgumentResolver.supportsParameter(any())).thenReturn(true);
+                when(teamMemberArgumentResolver.resolveArgument(any(), any(), any(), any()))
+                        .thenReturn(customOAuth2User);
+
                 when(informRepository.existsById(informId)).thenReturn(true);
-                doNothing().when(informCommandService).deleteInform(informId, 1L);
+                doNothing().when(informCommandService)
+                        .deleteInform(informId, member.getId());
             }
 
             @Test
             @DisplayName("204 NoContent를 반환한다.")
-            void it_returns_204_no_content()throws Exception{
+            void it_returns_204_no_content() throws Exception {
                 mockMvc.perform(delete("/api/v1/informs/{informId}", informId)
                                 .contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().isNoContent());
@@ -179,17 +206,29 @@ public class InformControllerTest {
 
     @Nested
     @DisplayName("getInform 메소드는")
-    class Describe_getInform{
+    class Describe_getInform {
 
         @Nested
         @DisplayName("inform이 존재하는 경우")
-        class Context_Exist_Inform{
+        class Context_Exist_Inform {
 
             GetInformResponse response;
+            Long informId;
+            Member member;
+            CustomOAuth2User customOAuth2User;
 
             @BeforeEach
-            void setUp() {
-                response = new GetInformResponse("test", "test", List.of(),10, List.of(), LocalDate.now());
+            void setUp() throws Exception {
+                informId = 1L;
+                response = new GetInformResponse("test", "test", List.of(), 10, List.of(), LocalDate.now());
+
+                member = createMember();
+                OAuth2UserDTO oAuth2UserDTO = OAuth2UserDTO.from(member);
+                customOAuth2User = new CustomOAuth2User(oAuth2UserDTO);
+
+                when(teamMemberArgumentResolver.supportsParameter(any())).thenReturn(true);
+                when(teamMemberArgumentResolver.resolveArgument(any(), any(), any(), any()))
+                        .thenReturn(customOAuth2User);
 
                 when(informRepository.existsById(informId)).thenReturn(true);
                 when(informQueryService.getInformById(customOAuth2User.getMemberId(), informId)).thenReturn(response);
@@ -197,10 +236,11 @@ public class InformControllerTest {
 
             @Test
             @DisplayName("200 Ok, 그리고 해당 inform의 내용을 반환한다.")
-            void it_returns_200_ok() throws Exception{
+            void it_returns_200_ok() throws Exception {
                 mockMvc.perform(get("/api/v1/informs/{informId}", informId)
                                 .contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.data.title", equalTo(response.title())))
                         .andDo(print());
             }
         }
@@ -208,27 +248,41 @@ public class InformControllerTest {
 
     @Nested
     @DisplayName("addComment 메소드는")
-    class Describe_addComment{
+    class Describe_addComment {
 
         @Nested
         @DisplayName("유효한 요청이 들어왔을 경우")
-        class Context_Valid_Request{
+        class Context_Valid_Request {
 
             CreateInformCommentRequest request;
             CreateInformCommentResponse response;
+            Long informId;
+            Member member;
+            CustomOAuth2User customOAuth2User;
 
             @BeforeEach
-            void setUp() {
+            void setUp() throws Exception {
+                informId = 1L;
+
                 request = new CreateInformCommentRequest("test");
-                response = new CreateInformCommentResponse(1L, LocalDateTime.now());
+                response = new CreateInformCommentResponse(informId, LocalDateTime.now());
+
+                member = createMember();
+                OAuth2UserDTO oAuth2UserDTO = OAuth2UserDTO.from(member);
+                customOAuth2User = new CustomOAuth2User(oAuth2UserDTO);
+
+                when(teamMemberArgumentResolver.supportsParameter(any())).thenReturn(true);
+                when(teamMemberArgumentResolver.resolveArgument(any(), any(), any(), any()))
+                        .thenReturn(customOAuth2User);
 
                 when(informRepository.existsById(informId)).thenReturn(true);
-                when(informCommandService.addComment(informId, customOAuth2User.getMemberId(), request)).thenReturn(response);
+                when(informCommandService.addComment(informId, customOAuth2User.getMemberId(), request)).thenReturn(
+                        response);
             }
 
             @Test
             @DisplayName("201 Created를 반환한다.")
-            void it_returns_201_created()throws Exception{
+            void it_returns_201_created() throws Exception {
                 mockMvc.perform(post("/api/v1/informs/{informId}/comment", informId)
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(objectMapper.writeValueAsString(request)))
