0116cd3eda48e4c2f355225632c23595e16225aa
Ywoosang <opellong13@gmail.com>
2024-11-03 20:31:16 +0900

    refactor: DTO 에 @Builder 추가

--- yellobook-api/src/main/java/com/yellobook/domains/schedule/dto/response/SearchMonthlyScheduleResponse.java
+++ yellobook-api/src/main/java/com/yellobook/domains/schedule/dto/response/SearchMonthlyScheduleResponse.java
@@ -6,7 +6,7 @@ import java.util.List;
 import lombok.Builder;
 
 @Builder
-@Schema(description = "월별 키워드에 해당하는 일정 응답")
+@Schema(description = "월별 키워드에 해당하는 일정 목록 응답")
 public record SearchMonthlyScheduleResponse(
         @Schema(description = "일정 목록")
         List<QuerySchedule> schedules
--- yellobook-domain/src/main/java/com/yellobook/domains/schedule/dto/query/QueryUpcomingSchedule.java
+++ yellobook-domain/src/main/java/com/yellobook/domains/schedule/dto/query/QueryUpcomingSchedule.java
@@ -2,7 +2,9 @@ package com.yellobook.domains.schedule.dto.query;
 
 import com.yellobook.common.enums.ScheduleType;
 import java.time.LocalDate;
+import lombok.Builder;
 
+@Builder
 public record QueryUpcomingSchedule(
         String title,
         LocalDate day,
