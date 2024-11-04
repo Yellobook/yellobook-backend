0ce8b9269fdd2af345418196f13292fdb77130b5
jorippppong <duelee75@gmail.com>
2024-11-04 13:32:06 +0900

    feat(YB-578): updatedAt 필드 수정 메소드 추가

--- yellobook-domain/src/main/java/com/yellobook/domains/inventory/repository/InventoryRepository.java
+++ yellobook-domain/src/main/java/com/yellobook/domains/inventory/repository/InventoryRepository.java
@@ -1,11 +1,20 @@
 package com.yellobook.domains.inventory.repository;
 
 import com.yellobook.domains.inventory.entity.Inventory;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long>, InventoryRepositoryCustom {
     Optional<Inventory> findFirstByTeamIdOrderByCreatedAtDesc(Long teamId);
+
+    @Modifying
+    @Query("update Inventory i set i.updatedAt = :now where i.id = :id")
+    LocalDateTime updateUpdatedAt(@Param("id") Long id, @Param("now") LocalDateTime now);
+
 }
