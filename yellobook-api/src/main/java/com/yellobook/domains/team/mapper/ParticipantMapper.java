cd4568747e4d13dbd07fc6aeb048b86b5c2afc17
Ywoosang <opellong13@gmail.com>
2024-11-04 12:42:35 +0900

    remove: ParticipantMapper 에서 불필요한 메서드 제거

--- yellobook-api/src/main/java/com/yellobook/domains/team/mapper/ParticipantMapper.java
+++ yellobook-api/src/main/java/com/yellobook/domains/team/mapper/ParticipantMapper.java
@@ -2,19 +2,15 @@ package com.yellobook.domains.team.mapper;
 
 import com.yellobook.common.enums.TeamMemberRole;
 import com.yellobook.domains.member.entity.Member;
-import com.yellobook.domains.team.dto.MentionDTO;
 import com.yellobook.domains.team.entity.Participant;
 import com.yellobook.domains.team.entity.Team;
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ParticipantMapper {
+    @Mapping(source = "member", target = "member")
+    @Mapping(source = "team", target = "team")
     @Mapping(source = "teamMemberRole", target = "teamMemberRole")
     Participant toParticipant(TeamMemberRole teamMemberRole, Team team, Member member);
-
-    default MentionDTO toMentionDTO(List<Long> participantIds) {
-        return new MentionDTO(participantIds);
-    }
 }
