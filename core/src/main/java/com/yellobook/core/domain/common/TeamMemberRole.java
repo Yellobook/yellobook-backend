c3473656775eaaa0d1851c2756298ab7e14ab249
Ywoosang <opellong13@gmail.com>
2025-01-07 14:17:20 +0900

    refactor(YB-608): 팀 사용자 권한 enum 리팩토링

---
+++ core/src/main/java/com/yellobook/core/domain/common/TeamMemberRole.java
@@ -0,0 +1,18 @@
+package com.yellobook.core.domain.common;
+
+public enum TeamMemberRole {
+    ADMIN("관리자"),
+    ORDERER("주문자"),
+    VIEWER("뷰어");
+
+    private final String description;
+
+    TeamMemberRole(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
