73fd82f8bcb2ceb60bce03524d51aa3e966386dd
Ywoosang <opellong13@gmail.com>
2025-01-30 22:22:25 +0900

    refactor(YB-625): @ConfigurationProperties 도입

--- api-server/src/main/java/com/yellobook/ApiServerApplication.java
+++ api-server/src/main/java/com/yellobook/ApiServerApplication.java
@@ -2,8 +2,10 @@ package com.yellobook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApiServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiServerApplication.class, args);
