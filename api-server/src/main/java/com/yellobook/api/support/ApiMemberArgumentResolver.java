26ad96a22392b68b03ccd83433575c79c9c3e1a9
Ywoosang <opellong13@gmail.com>
2025-01-30 23:03:53 +0900

    refactor(YB-631): MethodArgumentResolver 리팩토링 - 웹 계층 객체인 ApiMember 로 받도록 수정

---
+++ api-server/src/main/java/com/yellobook/api/support/ApiMemberArgumentResolver.java
@@ -0,0 +1,67 @@
+package com.yellobook.api.support;
+
+import com.yellobook.api.support.auth.AppMemberRole;
+import com.yellobook.api.support.auth.error.AuthErrorType;
+import com.yellobook.api.support.auth.error.AuthException;
+import com.yellobook.api.support.auth.security.CustomUserDetails;
+import com.yellobook.api.support.error.ApiErrorType;
+import com.yellobook.api.support.error.ApiException;
+import com.yellobook.core.domain.member.Member;
+import com.yellobook.core.domain.member.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class ApiMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    public ApiMemberArgumentResolver(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType() == ApiMember.class;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            log.error("인가 정보가 존재하지 않음");
+            throw new AuthException(AuthErrorType.ACCESS_DENIED);
+        }
+
+        if (!(authentication.getPrincipal() instanceof CustomUserDetails customUserDetails)) {
+            log.error("인증 객체가 CustomUserDetails 타입이 아님: {}", authentication.getPrincipal()
+                    .getClass());
+            throw new AuthException(AuthErrorType.ACCESS_DENIED);
+        }
+
+        Long memberId = customUserDetails.getMemberId();
+        AppMemberRole role = customUserDetails.getRole();
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> {
+                    log.error("인가되었지만, 존재하지 않는 사용자  - memberId: {}", memberId);
+                    return new ApiException(ApiErrorType.INTERNAL_SERVER_ERROR);
+                });
+        return new ApiMember(
+                memberId,
+                member.socialInfo(),
+                member.profileInfo(),
+                role
+        );
+    }
+}
