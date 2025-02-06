9667e6bdd0020a9812c03fb0f6192f7e2b07f94a
Ywoosang <opellong13@gmail.com>
2025-02-07 00:20:55 +0900

    chore: 불필요한 컴포넌트 스캔 제거

--- storage/db-core/src/test/java/com/yellobook/storage/db/CoreDbTestApplication.java
+++ storage/db-core/src/test/java/com/yellobook/storage/db/CoreDbTestApplication.java
@@ -2,10 +2,8 @@ package com.yellobook.storage.db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.yellobook.core", "com.yellobook.storage.db.core"})
 public class CoreDbTestApplication {
 
     public static void main(String[] args) {
